e689600817dd3e09dd50f78949dc6a1482b272c6
Dave Tapley <dukedave@gmail.com>
2019-03-26 20:51:13 -0700

    Handle initialState

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -4,7 +4,7 @@ import { INCREMENT_ENTHUSIASM } from '../constants/index';
 
 const initialState: StoreState = { enthusiasmLevel: 100 };
 
-export function reducer(state: StoreState, action: EnthusiasmAction): StoreState {
+export function reducer(state: StoreState = initialState, action: EnthusiasmAction): StoreState {
   switch (action.type) {
     case INCREMENT_ENTHUSIASM:
       return { ...state, enthusiasmLevel: state.enthusiasmLevel + 1 };
