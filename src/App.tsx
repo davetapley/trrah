12d9792b48df4cab20050c48e617a78ffae2ba6a
Dave Tapley <dukedave@gmail.com>
2019-03-28 16:06:38 -0700

    Move stuff in to one file
    I was following (1) which wanted containers in a separate file,
    but I found it more confusing to have to look in two places.
    
    (1) https://github.com/Microsoft/TypeScript-React-Starter#making-a-container

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Number from './containers/Number';
 
 import { StoreState } from './types/index';
 
-class App extends Component{
+class App extends Component {
   render() {
     return (
       <Section>
--- src/containers/Number.tsx
+++ src/containers/Number.tsx
@@ -1,9 +1,29 @@
-import CountNumber from '../components/CountNumber';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button, Tag } from 'react-bulma-components';
+
 import * as actions from '../actions/';
 import { StoreState } from '../types/index';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 
+// Props for the functional component
+export interface NumberProps {
+  number: number;
+  onIncrement?: () => void;
+}
+
+// The functional component itself
+const NumberComponent: React.FC<NumberProps> = ({ number, onIncrement }) => (
+  <div>
+    <Button color="primary" onClick={onIncrement}>Increment</Button>
+    <Tag color="warning">
+    {number}
+    </Tag>
+  </div>
+)
+
+// The Redux container mappings
 export function mapStateToProps({ enthusiasmLevel }: StoreState) {
   return {
     number: enthusiasmLevel,
@@ -16,4 +36,4 @@ export function mapDispatchToProps(dispatch: Dispatch<actions.EnthusiasmAction>)
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CountNumber);
+export default connect(mapStateToProps, mapDispatchToProps)(NumberComponent);
